c5a6a91b5e307fa9d6d7137c634d97d1d1bd65c6
Amirali Davary <55234721+amiralid@users.noreply.github.com>
2023-02-27 16:55:32 +0100

    Create Imports.js

---
+++ Imports.js
@@ -0,0 +1,16 @@
+var colorizedVis = {"min":0,"max":1,"palette":["0000ff","00ffff","ffff00","ff0000","ffffff"]},
+    geometry = 
+    /* color: #d63000 */
+    /* shown: false */
+    /* displayProperties: [
+      {
+        "type": "rectangle"
+      }
+    ] */
+    ee.Geometry.Polygon(
+        [[[-114.86719065502085, 36.60223579560992],
+          [-114.86719065502085, 35.982387512431075],
+          [-114.04870920970835, 35.982387512431075],
+          [-114.04870920970835, 36.60223579560992]]], null, false),
+    WaterVis_02 = {"min":0,"max":1,"palette":["ff702b"]},
+    WaterVis_01 = {"min":0,"max":1,"palette":["00b8ff"]};
